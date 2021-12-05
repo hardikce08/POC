9df5a4526f86e5b70d67988c1699dee3ed93b03a
Hardik <hardikce.08@gmail.com>
2021-12-06 00:00:56 +0530

    Move API URL and Token to Configfile

--- AzureAd Login Sample/Controllers/BaseController.cs
+++ AzureAd Login Sample/Controllers/BaseController.cs
@@ -11,7 +11,32 @@ namespace POC.Controllers
         public BaseController()
         {
         }
-     
+        private string _bearertoken;
+        public string BearerToken
+        {
+            get
+            {
+                if (_bearertoken == null)
+                {
+                    _bearertoken = System.Configuration.ConfigurationManager.AppSettings["BearerToken"].ToString();
+                }
+
+                return _bearertoken;
+            }
+        }
+        private string _apidomin;
+        public string ApiDomain
+        {
+            get
+            {
+                if (_apidomin == null)
+                {
+                    _apidomin = System.Configuration.ConfigurationManager.AppSettings["APIURL"].ToString();
+                }
+
+                return _apidomin;
+            }
+        }
         //private SessionProfile _currentProfile;
         //public SessionProfile CurrentProfile
         //{
