d5dd98f206b866974a4b9df3f566068bbd93ba30
Hardik <hardikce.08@gmail.com>
2021-12-13 07:55:38 +0530

    Fix HighChart Js issue and other few js issues

--- AzureAd Login Sample/Theme/Scripts/theme.js
+++ AzureAd Login Sample/Theme/Scripts/theme.js
@@ -51,352 +51,352 @@ window.theme = {};
 
 }).apply( this, [ jQuery ]);
 
-// Skeleton
-(function(theme, $) {
+//// Skeleton
+//(function(theme, $) {
 
-	'use strict';
+//	'use strict';
 
-	theme = theme || {};
-
-	var $body		= $( 'body' ),
-		$html		= $( 'html' ),
-		$window		= $( window ),
-		isAndroid	= navigator.userAgent.toLowerCase().indexOf('android') > -1;
+//	theme = theme || {};
+
+//	var $body		= $( 'body' ),
+//		$html		= $( 'html' ),
+//		$window		= $( window ),
+//		isAndroid	= navigator.userAgent.toLowerCase().indexOf('android') > -1;
 
-	// mobile devices with fixed has a lot of issues when focus inputs and others...
-	if ( typeof $.browser !== 'undefined' && $.browser.mobile && $html.hasClass('fixed') ) {
-		$html.removeClass( 'fixed' ).addClass( 'scroll' );
-	}
+//	// mobile devices with fixed has a lot of issues when focus inputs and others...
+//	if ( typeof $.browser !== 'undefined' && $.browser.mobile && $html.hasClass('fixed') ) {
+//		$html.removeClass( 'fixed' ).addClass( 'scroll' );
+//	}
 
-	var Skeleton = {
+//	var Skeleton = {
 
-		options: {
-			sidebars: {
-				menu: '#content-menu',
-				left: '#sidebar-left',
-				right: '#sidebar-right'
-			}
-		},
-
-		customScroll: ( !Modernizr.overflowscrolling && !isAndroid && $.fn.nanoScroller !== 'undefined'),
-
-		initialize: function() {
-			this
-				.setVars()
-				.build()
-				.events();
-		},
-
-		setVars: function() {
-			this.sidebars = {};
-
-			this.sidebars.left = {
-				$el: $( this.options.sidebars.left )
-			};
-
-			this.sidebars.right = {
-				$el: $( this.options.sidebars.right ),
-				isOpened: $html.hasClass( 'sidebar-right-opened' )
-			};
-
-			this.sidebars.menu = {
-				$el: $( this.options.sidebars.menu ),
-				isOpened: $html.hasClass( 'inner-menu-opened' )
-			};
-
-			return this;
-		},
-
-		build: function() {
-
-			if ( typeof $.browser !== 'undefined' && $.browser.mobile ) {
-				$html.addClass( 'mobile-device' );
-			} else {
-				$html.addClass( 'no-mobile-device' );
-			}
-
-			$html.addClass( 'custom-scroll' );
-			if ( this.customScroll ) {
-				this.buildSidebarLeft();
-				this.buildContentMenu();
-			}
-
-			this.buildSidebarRight();
-
-			return this;
-		},
-
-		events: function() {
-			if ( this.customScroll ) {
-				this.eventsSidebarLeft();
-			}
-
-			this.eventsSidebarRight();
-			this.eventsContentMenu();
-
-			if ( typeof $.browser !== 'undefined' && !this.customScroll && isAndroid ) {
-				this.fixScroll();
-			}
-
-			return this;
-		},
-
-		fixScroll: function() {
-			var _self = this;
-
-			$window
-				.on( 'sidebar-left-opened sidebar-right-toggle', function( e, data ) {
-					_self.preventBodyScrollToggle( data.added );
-				});
-		},
-
-		buildSidebarLeft: function() {
-			this.sidebars.left.$nano = this.sidebars.left.$el.find( '.nano' );
-
-			this.sidebars.left.$nano.nanoScroller({
-				alwaysVisible: true,
-				preventPageScrolling: true
-			});
-
-			return this;
-		},
-
-		eventsSidebarLeft: function() {
-
-			var $nano = this.sidebars.left.$nano;
-
-			var updateNanoScroll = function() {
-				if ( $.support.transition ) {
-					$nano.nanoScroller();
-					$nano
-						.one('bsTransitionEnd', updateNanoScroll)
-						.emulateTransitionEnd(150)
-				} else {
-					updateNanoScroll();
-				}
-			};
-
-			this.sidebars.left.$el
-				.on( 'click', function() {
-					updateNanoScroll();
-				});
-
-			$nano
-				.on( 'mouseenter', function() {
-					if ( $html.hasClass( 'sidebar-left-collapsed' ) ) {
-						$nano.nanoScroller();
-					}
-				})
-				.on( 'mouseleave', function() {
-					if ( $html.hasClass( 'sidebar-left-collapsed' ) ) {
-						$nano.nanoScroller();
-					}
-				});
-
-			return this;
-		},
-
-		buildSidebarRight: function() {
-			this.sidebars.right.isOpened = $html.hasClass( 'sidebar-right-opened' );
-
-			if ( this.customScroll ) {
-				this.sidebars.right.$nano = this.sidebars.right.$el.find( '.nano' );
-
-				this.sidebars.right.$nano.nanoScroller({
-					alwaysVisible: true,
-					preventPageScrolling: true
-				});
-			}
-
-			return this;
-		},
-
-		eventsSidebarRight: function() {
-			var _self = this;
+//		options: {
+//			sidebars: {
+//				menu: '#content-menu',
+//				left: '#sidebar-left',
+//				right: '#sidebar-right'
+//			}
+//		},
+
+//		customScroll: ( !Modernizr.overflowscrolling && !isAndroid && $.fn.nanoScroller !== 'undefined'),
+
+//		initialize: function() {
+//			this
+//				.setVars()
+//				.build()
+//				.events();
+//		},
+
+//		setVars: function() {
+//			this.sidebars = {};
+
+//			this.sidebars.left = {
+//				$el: $( this.options.sidebars.left )
+//			};
+
+//			this.sidebars.right = {
+//				$el: $( this.options.sidebars.right ),
+//				isOpened: $html.hasClass( 'sidebar-right-opened' )
+//			};
+
+//			this.sidebars.menu = {
+//				$el: $( this.options.sidebars.menu ),
+//				isOpened: $html.hasClass( 'inner-menu-opened' )
+//			};
+
+//			return this;
+//		},
+
+//		build: function() {
+
+//			if ( typeof $.browser !== 'undefined' && $.browser.mobile ) {
+//				$html.addClass( 'mobile-device' );
+//			} else {
+//				$html.addClass( 'no-mobile-device' );
+//			}
+
+//			$html.addClass( 'custom-scroll' );
+//			if ( this.customScroll ) {
+//				this.buildSidebarLeft();
+//				this.buildContentMenu();
+//			}
+
+//			this.buildSidebarRight();
+
+//			return this;
+//		},
+
+//		events: function() {
+//			if ( this.customScroll ) {
+//				this.eventsSidebarLeft();
+//			}
+
+//			this.eventsSidebarRight();
+//			this.eventsContentMenu();
+
+//			if ( typeof $.browser !== 'undefined' && !this.customScroll && isAndroid ) {
+//				this.fixScroll();
+//			}
+
+//			return this;
+//		},
+
+//		fixScroll: function() {
+//			var _self = this;
+
+//			$window
+//				.on( 'sidebar-left-opened sidebar-right-toggle', function( e, data ) {
+//					_self.preventBodyScrollToggle( data.added );
+//				});
+//		},
+
+//		buildSidebarLeft: function() {
+//			//this.sidebars.left.$nano = this.sidebars.left.$el.find( '.nano' );
+
+//			//this.sidebars.left.$nano.nanoScroller({
+//			//	alwaysVisible: true,
+//			//	preventPageScrolling: true
+//			//});
+
+//			//return this;
+//		},
+
+//		eventsSidebarLeft: function() {
+
+//			var $nano = this.sidebars.left.$nano;
+
+//			var updateNanoScroll = function() {
+//				if ( $.support.transition ) {
+//					$nano.nanoScroller();
+//					$nano
+//						.one('bsTransitionEnd', updateNanoScroll)
+//						.emulateTransitionEnd(150)
+//				} else {
+//					updateNanoScroll();
+//				}
+//			};
+
+//			this.sidebars.left.$el
+//				.on( 'click', function() {
+//					updateNanoScroll();
+//				});
+
+//			$nano
+//				.on( 'mouseenter', function() {
+//					if ( $html.hasClass( 'sidebar-left-collapsed' ) ) {
+//						$nano.nanoScroller();
+//					}
+//				})
+//				.on( 'mouseleave', function() {
+//					if ( $html.hasClass( 'sidebar-left-collapsed' ) ) {
+//						$nano.nanoScroller();
+//					}
+//				});
+
+//			return this;
+//		},
+
+//		buildSidebarRight: function() {
+//			this.sidebars.right.isOpened = $html.hasClass( 'sidebar-right-opened' );
+
+//			if ( this.customScroll ) {
+//				this.sidebars.right.$nano = this.sidebars.right.$el.find( '.nano' );
+
+//				this.sidebars.right.$nano.nanoScroller({
+//					alwaysVisible: true,
+//					preventPageScrolling: true
+//				});
+//			}
+
+//			return this;
+//		},
+
+//		eventsSidebarRight: function() {
+//			var _self = this;
 
-			var open = function() {
-				if ( _self.sidebars.right.isOpened ) {
-					return close();
-				}
+//			var open = function() {
+//				if ( _self.sidebars.right.isOpened ) {
+//					return close();
+//				}
 
-				_self.sidebars.right.isOpened = true;
+//				_self.sidebars.right.isOpened = true;
 
-				$html.addClass( 'sidebar-right-opened' );
+//				$html.addClass( 'sidebar-right-opened' );
 
-				$window.trigger( 'sidebar-right-toggle', {
-					added: true,
-					removed: false
-				});
+//				$window.trigger( 'sidebar-right-toggle', {
+//					added: true,
+//					removed: false
+//				});
 
-				$html.on( 'click.close-right-sidebar', function(e) {
-					e.stopPropagation();
-					close(e);
-				});
-			};
+//				$html.on( 'click.close-right-sidebar', function(e) {
+//					e.stopPropagation();
+//					close(e);
+//				});
+//			};
 
-			var close = function(e) {
-				if ( !!e && !!e.target && ($(e.target).closest( '.sidebar-right' ).get(0) || !$(e.target).closest( 'html' ).get(0)) ) {
-					e.preventDefault();
-					return false;
-				}
+//			var close = function(e) {
+//				if ( !!e && !!e.target && ($(e.target).closest( '.sidebar-right' ).get(0) || !$(e.target).closest( 'html' ).get(0)) ) {
+//					e.preventDefault();
+//					return false;
+//				}
 
-				$html.removeClass( 'sidebar-right-opened' );
-				$html.off( 'click.close-right-sidebar' );
+//				$html.removeClass( 'sidebar-right-opened' );
+//				$html.off( 'click.close-right-sidebar' );
 
-				$window.trigger( 'sidebar-right-toggle', {
-					added: false,
-					removed: true
-				});
+//				$window.trigger( 'sidebar-right-toggle', {
+//					added: false,
+//					removed: true
+//				});
 
-				_self.sidebars.right.isOpened = false;
-			};
+//				_self.sidebars.right.isOpened = false;
+//			};
 
-			var bind = function() {
-				$('[data-open="sidebar-right"]').on('click', function(e) {
-					var $el = $(this);
-					e.stopPropagation();
+//			var bind = function() {
+//				$('[data-open="sidebar-right"]').on('click', function(e) {
+//					var $el = $(this);
+//					e.stopPropagation();
 
-					if ( $el.is('a') )
-						e.preventDefault();
-
-					open();
-				});
-			};
-
-			this.sidebars.right.$el.find( '.mobile-close' )
-				.on( 'click', function( e ) {
-					e.preventDefault();
-					$html.trigger( 'click.close-right-sidebar' );
-				});
-
-			bind();
-
-			return this;
-		},
-
-		buildContentMenu: function() {
-			if ( !$html.hasClass( 'fixed' ) ) {
-				return false;
-			}
-
-			this.sidebars.menu.$nano = this.sidebars.menu.$el.find( '.nano' );
-
-			this.sidebars.menu.$nano.nanoScroller({
-				alwaysVisible: true,
-				preventPageScrolling: true
-			});
-
-			return this;
-		},
-
-		eventsContentMenu: function() {
-			var _self = this;
-
-			var open = function() {
-				if ( _self.sidebars.menu.isOpened ) {
-					return close();
-				}
-
-				_self.sidebars.menu.isOpened = true;
-
-				$html.addClass( 'inner-menu-opened' );
-
-				$window.trigger( 'inner-menu-toggle', {
-					added: true,
-					removed: false
-				});
-
-				$html.on( 'click.close-inner-menu', function(e) {
-
-					close(e);
-				});
-
-			};
-
-			var close = function(e) {
-				if ( !!e && !!e.target && !$(e.target).closest( '.inner-menu-collapse' ).get(0) && ($(e.target).closest( '.inner-menu' ).get(0) || !$(e.target).closest( 'html' ).get(0)) ) {
-					return false;
-				}
-
-				e.stopPropagation();
-
-				$html.removeClass( 'inner-menu-opened' );
-				$html.off( 'click.close-inner-menu' );
-
-				$window.trigger( 'inner-menu-toggle', {
-					added: false,
-					removed: true
-				});
-
-				_self.sidebars.menu.isOpened = false;
-			};
-
-			var bind = function() {
-				$('[data-open="inner-menu"]').on('click', function(e) {
-					var $el = $(this);
-					e.stopPropagation();
-
-					if ( $el.is('a') )
-						e.preventDefault();
-
-					open();
-				});
-			};
-
-			bind();
-
-			/* Nano Scroll */
-			if ( $html.hasClass( 'fixed' ) ) {
-				var $nano = this.sidebars.menu.$nano;
-
-				var updateNanoScroll = function() {
-					if ( $.support.transition ) {
-						$nano.nanoScroller();
-						$nano
-							.one('bsTransitionEnd', updateNanoScroll)
-							.emulateTransitionEnd(150)
-					} else {
-						updateNanoScroll();
-					}
-				};
-
-				this.sidebars.menu.$el
-					.on( 'click', function() {
-						updateNanoScroll();
-					});
-			}
-
-			return this;
-		},
-
-		preventBodyScrollToggle: function( shouldPrevent, $el ) {
-			setTimeout(function() {
-				if ( shouldPrevent ) {
-					$body
-						.data( 'scrollTop', $body.get(0).scrollTop )
-						.css({
-							position: 'fixed',
-							top: $body.get(0).scrollTop * -1
-						})
-				} else {
-					$body
-						.css({
-							position: '',
-							top: ''
-						})
-						.scrollTop( $body.data( 'scrollTop' ) );
-				}
-			}, 150);
-		}
-
-	};
-
-	// expose to scope
-	$.extend(theme, {
-		Skeleton: Skeleton
-	});
-
-}).apply(this, [ window.theme, jQuery ]);
+//					if ( $el.is('a') )
+//						e.preventDefault();
+
+//					open();
+//				});
+//			};
+
+//			this.sidebars.right.$el.find( '.mobile-close' )
+//				.on( 'click', function( e ) {
+//					e.preventDefault();
+//					$html.trigger( 'click.close-right-sidebar' );
+//				});
+
+//			bind();
+
+//			return this;
+//		},
+
+//		buildContentMenu: function() {
+//			if ( !$html.hasClass( 'fixed' ) ) {
+//				return false;
+//			}
+
+//			this.sidebars.menu.$nano = this.sidebars.menu.$el.find( '.nano' );
+
+//			this.sidebars.menu.$nano.nanoScroller({
+//				alwaysVisible: true,
+//				preventPageScrolling: true
+//			});
+
+//			return this;
+//		},
+
+//		eventsContentMenu: function() {
+//			var _self = this;
+
+//			var open = function() {
+//				if ( _self.sidebars.menu.isOpened ) {
+//					return close();
+//				}
+
+//				_self.sidebars.menu.isOpened = true;
+
+//				$html.addClass( 'inner-menu-opened' );
+
+//				$window.trigger( 'inner-menu-toggle', {
+//					added: true,
+//					removed: false
+//				});
+
+//				$html.on( 'click.close-inner-menu', function(e) {
+
+//					close(e);
+//				});
+
+//			};
+
+//			var close = function(e) {
+//				if ( !!e && !!e.target && !$(e.target).closest( '.inner-menu-collapse' ).get(0) && ($(e.target).closest( '.inner-menu' ).get(0) || !$(e.target).closest( 'html' ).get(0)) ) {
+//					return false;
+//				}
+
+//				e.stopPropagation();
+
+//				$html.removeClass( 'inner-menu-opened' );
+//				$html.off( 'click.close-inner-menu' );
+
+//				$window.trigger( 'inner-menu-toggle', {
+//					added: false,
+//					removed: true
+//				});
+
+//				_self.sidebars.menu.isOpened = false;
+//			};
+
+//			var bind = function() {
+//				$('[data-open="inner-menu"]').on('click', function(e) {
+//					var $el = $(this);
+//					e.stopPropagation();
+
+//					if ( $el.is('a') )
+//						e.preventDefault();
+
+//					open();
+//				});
+//			};
+
+//			bind();
+
+//			/* Nano Scroll */
+//			if ( $html.hasClass( 'fixed' ) ) {
+//				var $nano = this.sidebars.menu.$nano;
+
+//				var updateNanoScroll = function() {
+//					if ( $.support.transition ) {
+//						$nano.nanoScroller();
+//						$nano
+//							.one('bsTransitionEnd', updateNanoScroll)
+//							.emulateTransitionEnd(150)
+//					} else {
+//						updateNanoScroll();
+//					}
+//				};
+
+//				this.sidebars.menu.$el
+//					.on( 'click', function() {
+//						updateNanoScroll();
+//					});
+//			}
+
+//			return this;
+//		},
+
+//		preventBodyScrollToggle: function( shouldPrevent, $el ) {
+//			setTimeout(function() {
+//				if ( shouldPrevent ) {
+//					$body
+//						.data( 'scrollTop', $body.get(0).scrollTop )
+//						.css({
+//							position: 'fixed',
+//							top: $body.get(0).scrollTop * -1
+//						})
+//				} else {
+//					$body
+//						.css({
+//							position: '',
+//							top: ''
+//						})
+//						.scrollTop( $body.data( 'scrollTop' ) );
+//				}
+//			}, 150);
+//		}
+
+//	};
+
+//	// expose to scope
+//	$.extend(theme, {
+//		Skeleton: Skeleton
+//	});
+
+//}).apply(this, [ window.theme, jQuery ]);
 
 // Base
 (function(theme, $) {
@@ -405,7 +405,7 @@ window.theme = {};
 
 	theme = theme || {};
 
-	theme.Skeleton.initialize();
+	//theme.Skeleton.initialize();
 
 }).apply(this, [ window.theme, jQuery ]);
 
