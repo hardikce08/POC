dc6f7c0857c780490dce8e362a48b5f110808771
Hardik <hardikce.08@gmail.com>
2022-01-11 11:05:17 +0530

    minor fix in Username

--- AzureAd Login Sample/Controllers/HomeController.cs
+++ AzureAd Login Sample/Controllers/HomeController.cs
@@ -330,28 +330,18 @@ namespace POC.Controllers
         }
         public ActionResult HighChart()
         {
-            //if (Request.IsAuthenticated)
-            //{
-            //    var userClaims = User.Identity as System.Security.Claims.ClaimsIdentity;
-
-            //    //You get the user's first and last name below:
-            //    ViewBag.Name = userClaims?.FindFirst("name")?.Value;
-            //    ViewBag.UserGuid = userClaims?.FindFirst("aud")?.Value;
-            //    // The 'preferred_username' claim can be used for showing the username
-            //    ViewBag.Username = userClaims?.FindFirst("preferred_username")?.Value;
-
-            //    // The subject/ NameIdentifier claim can be used to uniquely identify the user across the web
-            //    ViewBag.Subject = userClaims?.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier)?.Value;
-
-            //    // TenantId is the unique Tenant Id - which represents an organization in Azure AD
-            //    ViewBag.TenantId = userClaims?.FindFirst("http://schemas.microsoft.com/identity/claims/tenantid")?.Value;
-            //    //Session["UserID"]= userClaims?.FindFirst("aud")?.Value;
-            //}
-            //else
-            //{
-            //    return RedirectToAction("Index", "Home");
-            //}
-            //TempData["Message"] = "test";
+            if (Request.Cookies["UserToken"] != null)
+            {
+                //You get the user's first and last name below:
+                ViewBag.Name = Request.Cookies["UserName"]?.Value;
+                ViewBag.UserGuid = Request.Cookies["UserGuid"]?.Value;
+                // The 'preferred_username' claim can be used for showing the username
+                ViewBag.Username = Request.Cookies["UserEmail"]?.Value;
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
             ViewBag.Page = "report";
 
             // string GetAllProductAPIURL = "https://www.mockachino.com/97fd072e-cfdf-45/v1/products?category=active&offset=0&count=100";
